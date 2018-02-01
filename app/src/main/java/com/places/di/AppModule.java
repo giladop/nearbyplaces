d0ec4258cfb659b441f7d3fbf020280a3ce80206
giladopher <gilad.opher@moovitapp.com>
2018-02-01 16:27:20 +0200

    update gradle

--- app/src/main/java/com/places/di/AppModule.java
+++ app/src/main/java/com/places/di/AppModule.java
@@ -10,9 +10,8 @@ import javax.inject.Singleton;
 
 import dagger.Module;
 import dagger.Provides;
-import retrofit2.GsonConverterFactory;
 import retrofit2.Retrofit;
-
+import retrofit2.converter.gson.GsonConverterFactory;
 
 
 /**
--- app/src/main/java/com/places/model/api/GoogleMapsApiImpl.java
+++ app/src/main/java/com/places/model/api/GoogleMapsApiImpl.java
@@ -6,6 +6,7 @@ import com.google.android.gms.maps.model.LatLng;
 
 import javax.inject.Inject;
 
+import retrofit2.Call;
 import retrofit2.Callback;
 import retrofit2.Response;
 import retrofit2.Retrofit;
@@ -43,15 +44,17 @@ public class GoogleMapsApiImpl{
 	public void getAddress(@NonNull LatLng location, @NonNull final RemoteRepository.GetAddressCallback callback){
 		String locationParam = location.latitude + "," + location.longitude;
 		api.getAddress(locationParam).enqueue(new Callback<Addresses>(){
+
 			@Override
-			public void onResponse(Response<Addresses> response){
+			public void onResponse(Call<Addresses> call, Response<Addresses> response) {
 				callback.onAddressLoaded(response.body());
 			}
 
 			@Override
-			public void onFailure(Throwable t){
+			public void onFailure(Call<Addresses> call, Throwable t) {
 				callback.onAddressNotAvailable();
 			}
+
 		});
 	}
 
@@ -62,13 +65,14 @@ public class GoogleMapsApiImpl{
 	public void getPlaces(@NonNull LatLng location, @NonNull final RemoteRepository.GetPlacesCallback callback){
 		String locationParam = location.latitude + "," + location.longitude;
 		api.getPlaces(locationParam).enqueue(new Callback<Places>(){
+
 			@Override
-			public void onResponse(Response<Places> response){
+			public void onResponse(Call<Places> call, Response<Places> response) {
 				callback.onPlacesLoaded(response.body());
 			}
 
 			@Override
-			public void onFailure(Throwable t){
+			public void onFailure(Call<Places> call, Throwable t) {
 				callback.onPlacesNotAvailable();
 			}
 		});
